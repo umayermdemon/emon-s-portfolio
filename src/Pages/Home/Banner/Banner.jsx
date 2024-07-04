4b1056141a8e24f79af5f22dc042bd7b9d70fa9d
EMON <mamudmdemon@gmail.com>
2024-07-04 18:10:58 +0600

    responsived banner section

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -4,17 +4,17 @@ import profileImg from "../../../assets/file.png";
 
 const Banner = () => {
   return (
-    <div className="mt-12 max-w-7xl mx-auto">
-      <div className="flex items-center flex-col lg:justify-between lg:flex-row">
-        <div className="flex-1 space-y-4">
-          <h1 className="text-2xl text-white font-bold font-poppins">
+    <div className="lg:mt-12 lg:max-w-7xl mx-auto">
+      <div className="flex items-center flex-col-reverse lg:justify-between lg:flex-row">
+        <div className="flex-1 space-y-3 lg:space-y-4 px-4 md:px-0 ml-0 md:ml-8 lg:ml-0">
+          <h1 className="text-base md:text-2xl text-white font-bold font-poppins">
             Hi, I'm{" "}
           </h1>
-          <h1 className="text-6xl text-white font-bold font-poppins">
+          <h1 className="text-3xl md:text-6xl text-white font-bold font-poppins">
             Md. Emon Miah{" "}
           </h1>
-          <h2 className="text-[#FFB641] font-poppins">Junior Web Developer</h2>
-          <p className="font-poppins text-white pb-4">
+          <h2 className="text-[#FFB641] text-sm md:text-base font-poppins">Junior Web Developer</h2>
+          <p className="font-poppins text-sm md:text-base text-white pb-4">
             Passionate about creating interactive and responsive web
             applications using modern technologies.
           </p>
@@ -28,10 +28,10 @@ const Banner = () => {
           </a>
         </div>
 
-        <div className="flex-1 pl-72">
+        <div className="flex-1 lg:pl-72">
           <img
             src={profileImg}
-            className="max-w-lg rounded-full bg-[#FFB641]"
+            className="max-w-xs lg:max-w-lg rounded-full bg-[#FFB641]"
           />
         </div>
       </div>
