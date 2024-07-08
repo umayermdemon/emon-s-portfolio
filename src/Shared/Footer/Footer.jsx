86f7be923fcf624a60c6e5244dbe8b3e82819a1b
EMON <mamudmdemon@gmail.com>
2024-07-08 17:53:31 +0600

    footer section modified

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
           >
             &copy; All Rights Reserved {currentYear} | Md.Emon Miah
           </Typography>
-          <div className="flex gap-4 text-white sm:justify-center">
+          <div className="flex gap-4 mt-4 text-white sm:justify-center">
             <ul className="my-1 flex gap-1 lg:mb-0 lg:mt-0 flex-row lg:items-center lg:gap-6">
               <Typography as="li" color="white" className="p-1 font-medium">
                 <a
